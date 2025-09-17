59890c11d699b21dbf5809f1dc99c019ce76040e
jackpoletek <arrayatbay@protonmail.com>
2025-09-17 18:23:10 +0100

    Edit Chat view, add URL for Chat

--- gr8tutor/views.py
+++ gr8tutor/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .models import Message
+from .models import Message, Tutor, Student, StudentTutorRelationship
 
 # Create your views here.
 def home(request):
@@ -27,14 +27,22 @@ def register(request):
 def chat_view(request, other_party_id):
     current_user = request.user
     other_user = User.objects.get(id=other_party_id)
-    
-sent_messages = Message.objects.filter(sender=current_user, recipient=other_user)
-received_messages = Message.objects.filter(sender=other_user, recipient=current_user)
 
+    sent_messages = Message.objects.filter(sender=current_user,
+                                           recipient=other_user)
+    received_messages = Message.objects.filter(sender=other_user,
+                                               recipient=current_user)
+    messages = sent_messages.union(received_messages).order_by("time")
 
-def index(request):
     if request.method == "POST":
-        return HttpResponse("You must have POSTed something")
-    else:
-        return HttpResponse(request.method)
+        text = request.POST.get("message")
+        if text:
+            Message.objects.create(sender=current_user, recipient=other_user,
+                                   text=text)
+            return redirect("chat", other_party_id=other_user.id)
+
+    return render(request, "gr8tutor/chat.html", {"messages": messages,
+                                                  "other_user": other_user})
+
+
 
--- gr8tutor_english_online/urls.py
+++ gr8tutor_english_online/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from gr8tutor import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gr8tutor.urls')),  # This includes all URLs from your app
+    path('', include('gr8tutor.urls')),
+    path('chat/<int:other_party_id>/', views.chat_view, name='chat'),
 ]
