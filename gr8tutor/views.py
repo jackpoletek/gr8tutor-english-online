a1aac482ee63d41d274c04e81f009b04d5c32c9a
jackpoletek <arrayatbay@protonmail.com>
2025-09-18 12:40:44 +0100

    Add check to chat view to ensure students and tutors can message each other

--- gr8tutor/views.py
+++ gr8tutor/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Message, Tutor, Student, StudentTutorRelationship
@@ -27,6 +28,22 @@ def register(request):
 def chat_view(request, other_party_id):
     current_user = request.user
     other_user = User.objects.get(id=other_party_id)
+
+    # Ensure that the current user and other user have a valid relationship
+    if not (
+        StudentTutorRelationship.objects.get(
+            tutor__user_profile__user=current_user,
+            student__user_profile__user=other_user,
+            is_active=True,
+        ).exists()
+        or
+        StudentTutorRelationship.objects.get(
+            tutor__user_profile__user=other_user,
+            student__user_profile__user=current_user,
+            is_active=True,
+        ).exists()
+    ):
+        return HttpResponseForbidden("Sorry, you aren't allowed to chat with this user.")
 
     sent_messages = Message.objects.filter(sender=current_user,
                                            recipient=other_user)
