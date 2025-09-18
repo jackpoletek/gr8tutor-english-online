3222a2b94cc21dc600588149c1064bfd5a5e6546
jackpoletek <arrayatbay@protonmail.com>
2025-09-18 14:16:14 +0100

    Register models in admin

--- gr8tutor/admin.py
+++ gr8tutor/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UserProfile, Tutor, Student, StudentTutorRelationship, Message
+
+admin.site.register(UserProfile)
+admin.site.register(Tutor)
+admin.site.register(Student)
+admin.site.register(StudentTutorRelationship)
+admin.site.register(Message)
