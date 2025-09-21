01fd221dcd3a6296de5e59cedc6338411659e6ac
jackpoletek <arrayatbay@protonmail.com>
2025-09-21 10:08:39 +0100

    Register models in admin.py

--- gr8tutor/admin.py
+++ gr8tutor/admin.py
@@ -8,3 +8,36 @@ admin.site.register(Tutor)
 admin.site.register(Student)
 admin.site.register(StudentTutorRelationship)
 admin.site.register(Message)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "role")
+    list_filter = ("role",)
+
+
+@admin.register(Tutor)
+class TutorAdmin(admin.ModelAdmin):
+    list_display = ("user_profile", "subject", "hourly_rate")
+    search_fields = ("user_profile__user__username", "subject")
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("user_profile", "goals")
+    search_fields = ("user_profile__user__username",)
+
+
+@admin.register(StudentTutorRelationship)
+class StudentTutorRelationshipAdmin(admin.ModelAdmin):
+    list_display = ("student", "tutor", "is_active")
+    list_filter = ("is_active",)
+    search_fields = ("student__user_profile__user__username",
+                     "tutor__user_profile__user__username")
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("sender", "recipient", "time", "text")
+    search_fields = ("sender__username", "recipient__username", "text")
+    list_filter = ("time",)
+    
