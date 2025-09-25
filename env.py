0f5e0ae290bbb73366e04e087df203624bdc102a
Jack <arrayatbay@protonmail.com>
2025-09-25 12:37:31 +0100

    Delete env.py

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL",
-    "postgresql://neondb_owner:npg_nusR3GgqCO7c@ep-lingering-snow-agphf6o4.c-2.eu-central-1.aws.neon.tech/ion_calm_churn_602459")
-
-os.environ.setdefault(
-    "SECRET_KEY", "django-key-xyz_d)j&jnlss(7k^2*k%&$!dpi3(2)6f-@5^zu5jfo)8g+%")
