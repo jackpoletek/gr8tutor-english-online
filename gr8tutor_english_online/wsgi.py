084bc9bfffe8002c4d44f7971689bde3eff8d4f2
jackpoletek <arrayatbay@protonmail.com>
2025-10-22 19:15:46 +0100

    Fix render.yaml, add psycopg2-binary==2.9.11 to requirements

--- gr8tutor_english_online/wsgi.py
+++ gr8tutor_english_online/wsgi.py
@@ -12,4 +12,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'gr8tutor_english_online.settings')
+
 application = get_wsgi_application()
