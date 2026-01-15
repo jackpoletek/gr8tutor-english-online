00f34cf7cd763f7ee04ddeb27b499e313ef60729
jackpoletek <arrayatbay@protonmail.com>
2026-01-15 22:39:27 +0000

    Fix DJANGO_SETTINGS_MODULE import path

--- gr8tutor_english_online/asgi.py
+++ gr8tutor_english_online/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gr8tutor_english_online.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'settings')
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault(
         'DJANGO_SETTINGS_MODULE',
-        'gr8tutor_english_online.settings'
+        'settings'
         )
     try:
         from django.core.management import execute_from_command_line
