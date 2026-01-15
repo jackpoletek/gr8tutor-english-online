49c2bc682b9ccbc3512104eff3db3b198b19b240
jackpoletek <arrayatbay@protonmail.com>
2026-01-15 22:27:09 +0000

    Fix WSGI import path

--- gr8tutor_english_online/wsgi.py
+++ gr8tutor_english_online/wsgi.py
@@ -7,12 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-import sys
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'gr8tutor_english_online.settings')
