16af8c9b15d5f2be07db22eefe36e9ee493c1711
jackpoletek <arrayatbay@protonmail.com>
2026-01-16 19:52:13 +0000

    Enforce namespacing for static files and update templates

--- gr8tutor_english_online/settings.py
+++ gr8tutor_english_online/settings.py
@@ -145,13 +145,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = "/static/"
 
-# Static files for production
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
 # Static files for development
 STATICFILES_DIRS = [
     BASE_DIR / "gr8tutor" / "static",
 ]
+
+# Static files for production
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # WhiteNoise configuration for serving static files
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
