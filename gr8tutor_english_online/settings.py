d4d8f0c6fa3a279a22be942fe78b7606e0487b9d
jackpoletek <arrayatbay@protonmail.com>
2026-01-16 12:32:26 +0000

    Edit settings

--- gr8tutor_english_online/settings.py
+++ gr8tutor_english_online/settings.py
@@ -39,17 +39,17 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 if DATABASE_URL:
     url = urlparse(DATABASE_URL)
     
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': url.path[1:],
-            'USER': url.username,
-            'PASSWORD': url.password,
-            'HOST': url.hostname,
-            'PORT': url.port,
-            }
-        }
-else:
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.postgresql',
+#            'NAME': url.path[1:],
+#            'USER': url.username,
+#            'PASSWORD': url.password,
+#            'HOST': url.hostname,
+#            'PORT': url.port,
+#            }
+#        }
+
     # Fallback to SQLite for local development
     DATABASES = {
         'default': {
